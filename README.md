6658b5a46ab649e2834241f864012bb64ff9bed4
ingedalvarez <ingedalvarez@gmail.com>
2022-03-05 11:26:14 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Swing App Calculator
+simple exercise using Java Swing in Intellij Idea
+https://cipsa.net/introduccion-ide-intellij-idea-apps-java-parte-2/
---
+++ src/main/java/calculator/Aplicacion.java
@@ -0,0 +1,27 @@
+package calculator;
+
+import javax.swing.*;
+
+public class Aplicacion {
+    private JPanel root;
+    private JPanel cabezera;
+    private JLabel lblDisplay;
+    private JButton btnReset;
+    private JPanel Botonera;
+    private JButton btnSiete;
+    private JButton btnOcho;
+    private JButton btnNueve;
+    private JButton btnDivision;
+    private JButton btnCuatro;
+    private JButton btnCinco;
+    private JButton btnSeis;
+    private JButton btnMultiplicacion;
+    private JButton btnUno;
+    private JButton btnPunto;
+    private JButton btnDos;
+    private JButton btnTres;
+    private JButton btnResta;
+    private JButton btnCero;
+    private JButton brnIgual;
+    private JButton btnSuma;
+}
